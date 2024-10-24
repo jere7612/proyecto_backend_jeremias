bcae054aefdc19319e8b4ed5a2817d7b063a174b
jere7612 <70173572+jere7612@users.noreply.github.com>
2024-10-23 21:03:53 -0500

    Carga inicial de mi proyecto personal

---
+++ src/main/java/pe/edu/cibertec/proyecto_backend_jeremias/ProyectoBackendJeremiasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.proyecto_backend_jeremias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackendJeremiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBackendJeremiasApplication.class, args);
+	}
+
+}
